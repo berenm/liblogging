3e416d91632bd41681210dd350625945ea419a80
Beren Minor <beren.minor+git@gmail.com>
2010-12-06 23:17:30 +0100

    Moved to boost-log trivial logging.

--- include/logging/logging.hpp
+++ include/logging/logging.hpp
@@ -6,54 +6,13 @@
 #ifndef LOGGER_HXX_
 #define LOGGER_HXX_
 
-#define BOOST_LOG_USE_BOOST_THREADS
+#define BOOST_FILESYSTEM_VERSION 2
 
-#include <boost/logging/format_fwd.hpp>
-#include <boost/logging/writer/on_dedicated_thread.hpp>
-
-#include <boost/logging/format_ts.hpp>
-#include <boost/logging/format/formatter/named_spacer.hpp>
-
-#include <boost/preprocessor.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/filters.hpp>
 
 namespace logging {
-  namespace bl = ::boost::logging;
-  namespace blt = ::boost::logging::tag;
-  namespace blo = ::boost::logging::optimize;
-  namespace blsu = ::boost::logging::scenario::usage;
-  namespace blsufc = ::boost::logging::scenario::usage::filter_::change;
-  namespace blsufl = ::boost::logging::scenario::usage::filter_::level;
-  namespace blsulc = ::boost::logging::scenario::usage::logger_::change;
-  namespace blsulf = ::boost::logging::scenario::usage::logger_::favor;
-  namespace blsulg = ::boost::logging::scenario::usage::logger_::gather;
-
-  struct dump_custom_levels {
-      static const ::boost::logging::char_type * dump(::boost::logging::level::type lvl);
-  };
-}
-
-namespace boost {
-  namespace logging {
-    template< > struct dump_level< override > {
-        typedef ::logging::dump_custom_levels type;
-    };
-  }
-}
-
-#include <boost/logging/format/formatter/tags.hpp>
-
-namespace logging {
-  typedef blt::holder< blo::cache_string_one_str< >, blt::file_line, blt::thread_id, blt::time, blt::function,
-      blt::module, blt::level > log_string;
-} // namespace logging
-
-BOOST_LOG_FORMAT_MSG(::logging::log_string)
-
-namespace logging {
-  typedef blsu::use< blsufc::set_once_when_multiple_threads, blsufl::use_levels,
-      blsulc::set_once_when_multiple_threads, blsulf::speed, blsulg::ostream_like > finder;
-  BOOST_DECLARE_LOG_FILTER(log_filter, finder::filter )
-  BOOST_DECLARE_LOG(logger, finder::logger)
 
   void init();
 
@@ -72,10 +31,6 @@ namespace logging {
 
 #define __logL(level)       __logML(main,level)
 
-#define __logML(module_m, level_m) BOOST_LOG_USE_LOG_IF_LEVEL(::logging::logger(), ::logging::log_filter(), level_m) \
-  .set_tag(BOOST_LOG_TAG_FILELINE) \
-  .set_tag(BOOST_LOG_TAG_FUNCTION) \
-  .set_tag(BOOST_LOG_TAG_LEVEL(level_m)) \
-  .set_tag(BOOST_LOG_TAG(module)( # module_m)) \
+#define __logML(module_m, level_m) BOOST_LOG_TRIVIAL(level_m) << #module_m \
 
 #endif /* LOGGER_HXX_ */
--- src/logging/logging.cpp
+++ src/logging/logging.cpp
@@ -5,91 +5,84 @@
  */
 
 #include "logging/logging.hpp"
-#include <boost/logging/format.hpp>
-#include <boost/logging/format/formatter/tags.hpp>
-#include <boost/logging/format/formatter/high_precision_time.hpp>
+
+#include <boost/log/sinks.hpp>
+#include <boost/log/utility/empty_deleter.hpp>
+
+#include <iostream>
+#include <fstream>
 
 namespace logging {
-  namespace blf = boost::logging::formatter;
+  namespace logging = ::boost::log;
+  namespace sinks = ::boost::log::sinks;
+  namespace src = ::boost::log::sources;
+  namespace fmt = ::boost::log::formatters;
+  namespace flt = ::boost::log::filters;
+  namespace attrs = ::boost::log::attributes;
+  namespace keywords = ::boost::log::keywords;
 
-  BOOST_DEFINE_LOG_FILTER(log_filter, finder::filter )
-  BOOST_DEFINE_LOG(logger, finder::logger)
+  //#define ANSI_ESCAPE() "\033["
+  //
+  //#define ANSI_CLEAR() ANSI_ESCAPE() "0m"
+  //
+  //#define ANSI_NORMAL()  ANSI_ESCAPE() "0;"
+  //#define ANSI_BRIGHT()  ANSI_ESCAPE() "1;"
+  //#define ANSI_FAINT()   ANSI_ESCAPE() "2;"
+  //#define ANSI_ITALIC()  ANSI_ESCAPE() "3;"
+  //
+  //#define ANSI_FG(mode_m, color_m) mode_m() "3" color_m() "m"
+  //#define ANSI_BG(mode_m, color_m) mode_m() "4" color_m() "m"
+  //
+  //#define ANSI_BLACK()   "0"
+  //#define ANSI_RED()     "1"
+  //#define ANSI_GREEN()   "2"
+  //#define ANSI_YELLOW()  "3"
+  //#define ANSI_BLUE()    "4"
+  //#define ANSI_MAGENTA() "5"
+  //#define ANSI_CYAN()    "6"
+  //#define ANSI_WHITE()   "7"
+  //
+  //#define ANSI_BOOST_LOG_STR(ansi_code_m, string) \
+//  (const ::boost::logging::char_type*) ::boost::logging::ansi_unicode_char_holder ( \
+//        ansi_code_m string, \
+//        L"" ansi_code_m string )
 
-#define ANSI_ESCAPE() "\033["
-
-#define ANSI_CLEAR() ANSI_ESCAPE() "0m"
-
-#define ANSI_NORMAL()  ANSI_ESCAPE() "0;"
-#define ANSI_BRIGHT()  ANSI_ESCAPE() "1;"
-#define ANSI_FAINT()   ANSI_ESCAPE() "2;"
-#define ANSI_ITALIC()  ANSI_ESCAPE() "3;"
-
-#define ANSI_FG(mode_m, color_m) mode_m() "3" color_m() "m"
-#define ANSI_BG(mode_m, color_m) mode_m() "4" color_m() "m"
-
-#define ANSI_BLACK()   "0"
-#define ANSI_RED()     "1"
-#define ANSI_GREEN()   "2"
-#define ANSI_YELLOW()  "3"
-#define ANSI_BLUE()    "4"
-#define ANSI_MAGENTA() "5"
-#define ANSI_CYAN()    "6"
-#define ANSI_WHITE()   "7"
-
-#define ANSI_BOOST_LOG_STR(ansi_code_m, string) \
-  (const ::boost::logging::char_type*) ::boost::logging::ansi_unicode_char_holder ( \
-        ansi_code_m string, \
-        L"" ansi_code_m string )
-
-  const ::boost::logging::char_type* dump_custom_levels::dump(::boost::logging::level::type lvl) {
-    using namespace ::boost::logging::level;
-    switch (lvl) {
-      case debug:
-        return ANSI_BOOST_LOG_STR( ANSI_FG(ANSI_NORMAL, ANSI_GREEN), "[debug] ");
-      case info:
-        return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_FAINT, ANSI_BLUE), "[info]  ");
-      case warning:
-        return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_NORMAL, ANSI_RED), "[warn]  ");
-      case error:
-        return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_FAINT, ANSI_MAGENTA), "[ERROR] ");
-      case fatal:
-        return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_BRIGHT, ANSI_RED), "[FATAL] ");
-      default:
-        return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_NORMAL, ANSI_BLACK), "");
-    }
-  }
-
-#undef ANSI_BOOST_LOG_STR
+  //  const ::boost::logging::char_type* dump_custom_levels::dump(::boost::logging::level::type lvl) {
+  //    using namespace ::boost::logging::level;
+  //    switch (lvl) {
+  //      case debug:
+  //      return ANSI_BOOST_LOG_STR( ANSI_FG(ANSI_NORMAL, ANSI_GREEN), "[debug] ");
+  //      case info:
+  //      return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_FAINT, ANSI_BLUE), "[info]  ");
+  //      case warning:
+  //      return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_NORMAL, ANSI_RED), "[warn]  ");
+  //      case error:
+  //      return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_FAINT, ANSI_MAGENTA), "[ERROR] ");
+  //      case fatal:
+  //      return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_BRIGHT, ANSI_RED), "[FATAL] ");
+  //      default:
+  //      return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_NORMAL, ANSI_BLACK), "");
+  //    }
+  //  }
+  //
+  //#undef ANSI_BOOST_LOG_STR
 
   void init() {
-    namespace bl = boost::logging;
-    namespace bld = boost::logging::destination;
-    namespace blf = boost::logging::formatter;
-    namespace blft = boost::logging::formatter::tag;
-    //         add formatters and destinations
-    //         That is, how the message is to be formatted and where should it be written to
 
-    blf::named_spacer_t< blf::do_convert_format::append > append(
-//        " (%idx%) %fileline% %function% [T%thread_id%]"
-        ANSI_CLEAR());
-    append.add("idx", blf::idx_t< blf::do_convert_format::append >());
-    append.add("thread_id", blft::thread_id_t< ::std::basic_ostringstream< bl::char_type >,
-        blf::do_convert_format::append >()); // thread_id tag
-    append.add("fileline", blft::file_line_t< blf::do_convert_format::append >());
-    append.add("function", blft::function_t< blf::do_convert_format::append >());
+    // Construct the sink
+    typedef sinks::synchronous_sink< sinks::text_ostream_backend > text_sink;
+    ::boost::shared_ptr< text_sink > sink_ptr = ::boost::make_shared< text_sink >();
 
-    blf::named_spacer prepend(ANSI_CLEAR() "%time% %level%%module%: ");
-    prepend.add("time", blf::high_precision_time("$hh:$mm:$ss.$micro")); // time tag
-    prepend.add("module", blf::tag::module());
-    prepend.add("level", blf::tag::level());
+    // Add a stream to write log to
+    sink_ptr->locked_backend()->add_stream(::boost::make_shared< ::std::ofstream >("console.log"));
 
-    logger()->writer().add_formatter(append); // file/line tag
-    logger()->writer().add_formatter(prepend); // file/line tag
+    // Register the sink in the logging core
+    logging::core::get()->add_sink(sink_ptr);
 
-    logger()->writer().add_formatter(blf::append_newline());
-    logger()->writer().add_destination(bld::cout());
-    //  g_l()->writer().add_destination( destination::file("out.txt") );
-    logger()->mark_as_initialized();
+    // We have to provide an empty deleter to avoid destroying the global stream object
+    ::boost::shared_ptr< ::std::ostream > stream_ptr(&::std::clog, logging::empty_deleter());
+    sink_ptr->locked_backend()->add_stream(stream_ptr);
+
   }
 
 } // namespace logging

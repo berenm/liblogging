fe953b7480eead340e11cbed8297d52da45fba19
Beren Minor <beren.minor+git@gmail.com>
2010-12-08 21:25:01 +0100

    Added color again.

--- include/logging/logging.hpp
+++ include/logging/logging.hpp
@@ -9,12 +9,51 @@
 #define BOOST_FILESYSTEM_VERSION 2
 
 #include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
+#include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
 #include <boost/log/filters.hpp>
+#include <boost/log/keywords/severity.hpp>
+#include <boost/log/formatters.hpp>
+#include <boost/log/attributes.hpp>
+#include <boost/log/trivial.hpp>
 
 namespace logging {
+  namespace logging = ::boost::log;
+  namespace sinks = ::boost::log::sinks;
+  namespace src = ::boost::log::sources;
+  namespace fmt = ::boost::log::formatters;
+  namespace flt = ::boost::log::filters;
+  namespace attrs = ::boost::log::attributes;
+  namespace keywords = ::boost::log::keywords;
+
+  //! Trivial severity levels
+  enum severity_level {
+    trace, debug, info, warning, error, fatal
+  };
+
+  char const* dump_level(severity_level lvl);
+
+  template< typename CharT, typename TraitsT >
+  inline std::basic_ostream< CharT, TraitsT >& operator<<(std::basic_ostream< CharT, TraitsT >& strm,
+                                                          severity_level lvl) {
+    strm << dump_level(lvl);
+    return strm;
+  }
 
   void init();
+
+  typedef src::severity_logger_mt< severity_level > logger_t;
+
+  BOOST_LOG_DECLARE_GLOBAL_LOGGER_INIT(logger, logger_t)
+  {
+    // do something on logger initialization and return logger instance
+    init();
+    return logger_t(keywords::severity = info);
+  }
+
+#define __logML(module_m, level_m) \
+            BOOST_LOG_STREAM_WITH_PARAMS(::logging::get_logger(), (::boost::log::keywords::severity = ::logging::level_m)) << "[" #module_m "] "
 
 } // namespace logging
 
@@ -30,7 +69,5 @@ namespace logging {
 #define __debug             __logML(main,debug)
 
 #define __logL(level)       __logML(main,level)
-
-#define __logML(module_m, level_m) BOOST_LOG_TRIVIAL(level_m) << "[" #module_m "] "
 
 #endif /* LOGGER_HXX_ */
--- src/logging/logging.cpp
+++ src/logging/logging.cpp
@@ -8,81 +8,80 @@
 
 #include <boost/log/sinks.hpp>
 #include <boost/log/utility/empty_deleter.hpp>
+#include <boost/log/attributes.hpp>
+
+#include <boost/log/utility/init/to_console.hpp>
+#include <boost/log/utility/init/to_file.hpp>
+#include <boost/log/utility/init/common_attributes.hpp>
+
+#include <boost/date_time.hpp>
 
 #include <iostream>
 #include <fstream>
 
 namespace logging {
-  namespace logging = ::boost::log;
-  namespace sinks = ::boost::log::sinks;
-  namespace src = ::boost::log::sources;
-  namespace fmt = ::boost::log::formatters;
-  namespace flt = ::boost::log::filters;
-  namespace attrs = ::boost::log::attributes;
-  namespace keywords = ::boost::log::keywords;
 
-  //#define ANSI_ESCAPE() "\033["
-  //
-  //#define ANSI_CLEAR() ANSI_ESCAPE() "0m"
-  //
-  //#define ANSI_NORMAL()  ANSI_ESCAPE() "0;"
-  //#define ANSI_BRIGHT()  ANSI_ESCAPE() "1;"
-  //#define ANSI_FAINT()   ANSI_ESCAPE() "2;"
-  //#define ANSI_ITALIC()  ANSI_ESCAPE() "3;"
-  //
-  //#define ANSI_FG(mode_m, color_m) mode_m() "3" color_m() "m"
-  //#define ANSI_BG(mode_m, color_m) mode_m() "4" color_m() "m"
-  //
-  //#define ANSI_BLACK()   "0"
-  //#define ANSI_RED()     "1"
-  //#define ANSI_GREEN()   "2"
-  //#define ANSI_YELLOW()  "3"
-  //#define ANSI_BLUE()    "4"
-  //#define ANSI_MAGENTA() "5"
-  //#define ANSI_CYAN()    "6"
-  //#define ANSI_WHITE()   "7"
-  //
-  //#define ANSI_BOOST_LOG_STR(ansi_code_m, string) \
-//  (const ::boost::logging::char_type*) ::boost::logging::ansi_unicode_char_holder ( \
-//        ansi_code_m string, \
-//        L"" ansi_code_m string )
+#define ANSI_ESCAPE() "\033["
 
-  //  const ::boost::logging::char_type* dump_custom_levels::dump(::boost::logging::level::type lvl) {
-  //    using namespace ::boost::logging::level;
-  //    switch (lvl) {
-  //      case debug:
-  //      return ANSI_BOOST_LOG_STR( ANSI_FG(ANSI_NORMAL, ANSI_GREEN), "[debug] ");
-  //      case info:
-  //      return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_FAINT, ANSI_BLUE), "[info]  ");
-  //      case warning:
-  //      return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_NORMAL, ANSI_RED), "[warn]  ");
-  //      case error:
-  //      return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_FAINT, ANSI_MAGENTA), "[ERROR] ");
-  //      case fatal:
-  //      return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_BRIGHT, ANSI_RED), "[FATAL] ");
-  //      default:
-  //      return ANSI_BOOST_LOG_STR(ANSI_FG(ANSI_NORMAL, ANSI_BLACK), "");
-  //    }
-  //  }
-  //
-  //#undef ANSI_BOOST_LOG_STR
+#define ANSI_CLEAR() ANSI_ESCAPE() "0m"
+
+#define ANSI_NORMAL()  ANSI_ESCAPE() "0;"
+#define ANSI_BRIGHT()  ANSI_ESCAPE() "1;"
+#define ANSI_FAINT()   ANSI_ESCAPE() "2;"
+#define ANSI_ITALIC()  ANSI_ESCAPE() "3;"
+
+#define ANSI_FG(mode_m, color_m) mode_m() "3" color_m() "m"
+#define ANSI_BG(mode_m, color_m) mode_m() "4" color_m() "m"
+
+#define ANSI_BLACK()   "0"
+#define ANSI_RED()     "1"
+#define ANSI_GREEN()   "2"
+#define ANSI_YELLOW()  "3"
+#define ANSI_BLUE()    "4"
+#define ANSI_MAGENTA() "5"
+#define ANSI_CYAN()    "6"
+#define ANSI_WHITE()   "7"
+
+  char const* dump_level(severity_level lvl) {
+    switch (lvl) {
+      case debug:
+        return ANSI_FG(ANSI_NORMAL, ANSI_GREEN) "[debug  ]";
+      case info:
+        return ANSI_FG(ANSI_FAINT, ANSI_BLUE) "[info   ]";
+      case warning:
+        return ANSI_FG(ANSI_NORMAL, ANSI_RED) "[warning]";
+      case error:
+        return ANSI_FG(ANSI_FAINT, ANSI_MAGENTA) "[ERROR  ]";
+      case fatal:
+        return ANSI_FG(ANSI_BRIGHT, ANSI_RED) "[FATAL  ]";
+      default:
+        return ANSI_FG(ANSI_NORMAL, ANSI_BLACK) "";
+    }
+  }
 
   void init() {
 
-    // Construct the sink
-    typedef sinks::synchronous_sink< sinks::text_ostream_backend > text_sink;
-    ::boost::shared_ptr< text_sink > sink_ptr = ::boost::make_shared< text_sink >();
+    logging::add_common_attributes();
 
-    // Add a stream to write log to
-    sink_ptr->locked_backend()->add_stream(::boost::make_shared< ::std::ofstream >("console.log"));
+    boost::shared_ptr< logging::core > core = logging::core::get();
 
-    // Register the sink in the logging core
-    logging::core::get()->add_sink(sink_ptr);
+    // Create a backend and attach a couple of streams to it
+    ::boost::shared_ptr< sinks::text_ostream_backend > backend = boost::make_shared< sinks::text_ostream_backend >();
+    backend->add_stream(::boost::shared_ptr< std::ostream >(&::std::clog, logging::empty_deleter()));
+    backend->set_formatter(fmt::format(ANSI_CLEAR() "%1%: %2% %3% %4%" ANSI_CLEAR())
+        % fmt::attr< unsigned int >("LineID", keywords::format = "%08x")
+        % fmt::date_time< ::boost::posix_time::ptime >("TimeStamp")
+        % fmt::attr< ::logging::severity_level >("Severity") % fmt::message());
 
-    // We have to provide an empty deleter to avoid destroying the global stream object
-    ::boost::shared_ptr< ::std::ostream > stream_ptr(&::std::clog, logging::empty_deleter());
-    sink_ptr->locked_backend()->add_stream(stream_ptr);
+    // Enable auto-flushing after each log record written
+    backend->auto_flush(true);
 
+    // Wrap it into the frontend and register in the core.
+    // The backend requires synchronization in the frontend.
+    typedef sinks::synchronous_sink< sinks::text_ostream_backend > sink_t;
+    boost::shared_ptr< sink_t > sink(new sink_t(backend));
+
+    core->add_sink(sink);
   }
 
 } // namespace logging
